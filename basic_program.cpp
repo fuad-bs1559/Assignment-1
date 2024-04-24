1219ce7dfcb301ad2ebc7329a43a2d239f08d950
fuadul hasan <fuadul.hasan@brainstation-23.com>
2024-04-24 19:46:04 +0600

    write factoiral of a number of basic program file

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"hello world"<<endl;
+
+    // basic program for calculating factorial
+    int n;
+    cin>>n;
+    int factorial = 1;
+    for(int i=1;i<=n;i++){
+        factorial *= i;
+    }
+    cout<<factorial<<endl;
     return 0;
 } 
