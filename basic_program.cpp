8cd2b00709a03f57352e85ff24a3a383b5c6cd8a
fuadul hasan <fuadul.hasan@brainstation-23.com>
2024-04-25 13:04:10 +0600

    cherry-pick 5ef276f commit and merge with incoming file

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,11 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int fibo(int n){
-    int ans = 1;
-    if(n < 1)return ans = max(0 , 1);
-    return ans += fibo(n-1)+fibo(n-2);
-    return ans;
+int nthPrime(int n) {
+    std::vector<bool> isPrime(1000000, true); // Assuming the prime number won't exceed 1000000
+    int count = 0;
+    for (int i = 2; i < isPrime.size(); ++i) {
+        if (isPrime[i]) {
+            ++count;
+            if (count == n) {
+                return i;
+            }
+            for (int j = i * 2; j < isPrime.size(); j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    return -1; // Return -1 if the nth prime is not found
 }
 
 int main(){
@@ -13,13 +23,15 @@ int main(){
     // this is the experimental-branch
     // we have to calculate fibonacci serise...
 
-    // basic program for calculating factorial
-    // this is an another test in the experimental branch
     int n;
-    cin>>n;
-    int num = fibo(n);
-    cout<<num<<endl;
-    // adding comment in main branch to arrise conflict....
-    // this is another comment adding for conflict...
+    std::cout << "Enter the value of n: ";
+    std::cin >> n;
+    
+    int result = nthPrime(n);
+    if (result != -1) {
+        std::cout << "The " << n << "th prime number is: " << result << std::endl;
+    } else {
+        std::cout << "The " << n << "th prime number is not found within the given limit." << std::endl;
+    }
     return 0;
 } 
