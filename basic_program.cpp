89f69ac01f00ef15b59fb6326af668af8ce93306
fuadul hasan <fuadul.hasan@brainstation-23.com>
2024-04-25 14:58:46 +0600

    reset with the previous prgramm

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"hello world"<<endl;
+
+    // two number summation code added...
+    int a,b;
+    cin>>a>>b;
+
+    cout<<a+b<<endl;
     return 0;
 } 
